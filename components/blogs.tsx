133973524d6f2204080e38ebd663e9679bd5f16b
umamaheshdev01 <139320517+umamaheshdev01@users.noreply.github.com>
2023-10-18 17:08:40 +0530

    Ok

--- components/blogs.tsx
+++ components/blogs.tsx
@@ -15,7 +15,7 @@ export default function Blogs() {
       <div>
         {blogsData.map((project, index) => (
           <React.Fragment key={index}>
-           <a href='/'><Project {...project} /></a> 
+           <Project {...project} ><a href='/'></a></Project>
           </React.Fragment>
         ))}
       </div>
