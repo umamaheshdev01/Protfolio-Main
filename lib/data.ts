128f40e574b3be1bba1a28e896dfbdf1f6d453f0
umamaheshdev01 <139320517+umamaheshdev01@users.noreply.github.com>
2023-10-25 01:20:51 +0530

    Hi

--- lib/data.ts
+++ lib/data.ts
@@ -28,10 +28,6 @@ export const links = [
     hash: "#experience",
   },
   {
-    name : "Blogs",
-    hash : "#blogs"
-  },
-  {
     name: "Contact",
     hash: "#contact",
   },
@@ -39,36 +35,30 @@ export const links = [
 
 export const experiencesData = [
   {
-    title: "Class 12",
-    location: "Hyderabad",
+    title: "Graduated bootcamp",
+    location: "Miami, FL",
     description:
-      "Completed my 12th at my home state Telanagna at Narayana group of schools.",
+      "I graduated after 6 months of studying. I immediately found a job as a front-end developer.",
     icon: React.createElement(LuGraduationCap),
-    date: "2022",
+    date: "2019",
   },
   {
-    title: "B-Tech",
-    location: "Kurukshetra",
+    title: "Front-End Developer",
+    location: "Orlando, FL",
     description:
-      "Currently pursuing my B-Tech 2nd year at NIT Kurukshetra.",
-    icon: React.createElement(LuGraduationCap),
-    date: "2023",
+      "I worked as a front-end developer for 2 years in 1 job and 1 year in another job. I also upskilled to the full stack.",
+    icon: React.createElement(CgWorkAlt),
+    date: "2019 - 2021",
   },
- 
-] as const;
-
-export const projectsData = [
   {
-    title: "Movie Ticket Website",
+    title: "Full-Stack Developer",
+    location: "Houston, TX",
     description:
-      "My First Project , Pure HTML CSS - Movie Tickect Booking website",
-    tags: ["HTML", "CSS", "Javascript"],
-    imageUrl: corpcommentImg,
-    link : "google.com"
-  }
+      "I'm now a full-stack developer working as a freelancer. My stack includes React, Next.js, TypeScript, Tailwind, Prisma and MongoDB. I'm open to full-time opportunities.",
+    icon: React.createElement(FaReact),
+    date: "2021 - present",
+  },
 ] as const;
-
-
 
 export const blogsData = [
   {
@@ -79,12 +69,24 @@ export const blogsData = [
     imageUrl: corpcommentImg,
   },
   {
-    title: "CorpComment",
+    title: "rmtDev",
     description:
-      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
-    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
-    imageUrl: corpcommentImg,
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
   },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+
+
+export const projectsData = [
   {
     title: "CorpComment",
     description:
@@ -92,23 +94,40 @@ export const blogsData = [
     tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
     imageUrl: corpcommentImg,
   },
- 
-  
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
 ] as const;
-
-
-
 
 export const skillsData = [
   "HTML",
   "CSS",
   "JavaScript",
+  "TypeScript",
   "React",
+  "Next.js",
   "Node.js",
   "Git",
   "Tailwind",
+  "Prisma",
   "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
   "Express",
+  "PostgreSQL",
   "Python",
-  "Django"
+  "Django",
+  "Framer Motion",
 ] as const;
