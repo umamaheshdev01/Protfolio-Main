a162fc6e87952fd5301940a768a2a8705de02c7d
umamaheshdev01 <139320517+umamaheshdev01@users.noreply.github.com>
2023-10-24 19:09:59 +0530

    Hi

--- components/intro.tsx
+++ components/intro.tsx
@@ -100,7 +100,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://linkedin.com"
+          href="www.linkedin.com/in/uma-mahesh-a4b089297"
           target="_blank"
         >
           <BsLinkedin />
@@ -108,7 +108,7 @@ export default function Intro() {
 
         <a
           className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
-          href="https://github.com"
+          href="https://github.com/umamaheshdev01"
           target="_blank"
         >
           <FaGithubSquare />
